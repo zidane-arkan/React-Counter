de94d63ea3c35a5942399c21d38be9845903704f
Zidane Arkan <muhammad.zidane@sera.astra.co.id>
2024-10-01 06:36:37 +0700

    Update

--- src/components/Counter/CounterOutput.jsx
+++ src/components/Counter/CounterOutput.jsx
@@ -3,6 +3,6 @@ import { log } from '../../log.js';
 export default function CounterOutput({ value }) {
   log('<CounterOutput /> rendered', 2);
 
-  const cssClass = value >= 0 ? 'counter-output' : 'counter-output negative';
+  const cssClass = value >= 0 ? 'counter-output' : 'counter-output negative ';
   return <span className={cssClass}>{value}</span>;
 }
