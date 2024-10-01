aa4631ffcf8dbac9827586ea437649cac644f50f
Zidane Arkan <muhammad.zidane@sera.astra.co.id>
2024-10-01 10:56:08 +0700

    Update React Counetr

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <header id="main-header">
       <img src={logoImg} alt="Magnifying glass analyzing a document" />
-      <h1>React - Behind The Scenes</h1>
+      <h1>React Counter - Behind The Scenes</h1>
     </header>
   );
 }
